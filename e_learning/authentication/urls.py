f074556586a5d08945f36ea84203acfe915e0ddd
dtnghia2010 <ititiu21256@student.hcmiu.edu.vn>
2024-03-14 10:04:00 +0700

    simple authentication system

---
+++ e_learning/authentication/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
+    path('', views.homePage, name="home"),
+]
