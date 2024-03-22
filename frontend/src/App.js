1889bd171c582407f71b9ec082caed8f8ebfdfe9
nathang0147 <nganhthang147@gmail.com>
2024-03-22 14:47:03 +0700

    feat: Router for Course

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,19 +9,23 @@ import Header from "./component/layout/Header";
 import Login from "./page/Login";
 import Register from "./page/Register";
 import HomePage from "./page/HomePage";
+import CourseList from "./page/course/CourseList";
+import Footer from "./component/layout/Footer";
 
 function App() {
 
   return (
       <div className="bg-gray-tone">
-          <Header />
       <BrowserRouter>
+          <Header />
         <Routes>
           <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
             <Route exact path="/" element={<HomePage />}/>
+            <Route path="/course" element={<CourseList/>}/>
         </Routes>
       </BrowserRouter>
+          <Footer/>
       </div>
   )
 }
