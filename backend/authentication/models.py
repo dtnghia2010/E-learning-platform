6bd5e8940c219acfeb9c5e77f9afc14022a4dc75
nathang0147 <nganhthang147@gmail.com>
2024-03-22 09:13:59 +0700

    Merge into main

---
+++ backend/authentication/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20, blank=True) #blank =True means this field is optional
+
+    #distinguish between built-in User model and custom User model
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
