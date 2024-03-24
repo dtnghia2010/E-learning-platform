ebee335817e87a8ae59d411a295e8006566bb292
thuongngo050902 <ngothithuong592002@gmail.com>
2024-03-24 15:13:04 +0700

    Course Model

---
+++ Course/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+    course_id = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=255)
+    document_id = models.IntegerField()
+    category_id = models.IntegerField()
+
+    def __str__(self):
+        return self.course_name
+class Document(models.Model):
+    document_id = models.AutoField(primary_key=True)
+    document_name = models.CharField(max_length=255)
+    description = models.TextField()
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='documents')
+
+    def __str__(self):
+        return self.document_name
