655f9be1b7625b2ec6f9bfb4cf18faf1c8aef8e4
thuongngo050902 <ngothithuong592002@gmail.com>
2024-03-24 15:11:30 +0700

    Course Model

--- authentication/models.py
+++ authentication/models.py
@@ -10,20 +10,3 @@ class User(AbstractUser):
     #distinguish between built-in User model and custom User model
     groups = models.ManyToManyField(Group, related_name='custom_user_groups')
     user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
-
-class Course(models.Model):
-    course_id = models.AutoField(primary_key=True)
-    course_name = models.CharField(max_length=255)
-    document_id = models.IntegerField()
-    category_id = models.IntegerField()
-
-    def __str__(self):
-        return self.course_name
-class Document(models.Model):
-    document_id = models.AutoField(primary_key=True)
-    document_name = models.CharField(max_length=255)
-    description = models.TextField()
-    course = models.ForeignKey('Course', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.document_name
