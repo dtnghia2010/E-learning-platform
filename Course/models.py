5c42a761e429ba78ebb6634b0f8e283bfae1e695
Khoa <khoakhoa2812@gmail.com>
2024-03-25 21:29:16 +0700

    temp commit

--- Course/models.py
+++ Course/models.py
@@ -23,14 +23,14 @@ class Chapter(models.Model):
     chapter_name = models.CharField(max_length=255)
     content = models.TextField()
     code = models.ForeignKey('Quiz', on_delete=models.CASCADE)
-    document = models.ForeignKey('Document', on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='chapters')
 
     def __str__(self):
         return self.chapter_name
 
 class Bookmark(models.Model):
     user_id = models.AutoField(primary_key=True)
-    document = models.ForeignKey('Document', on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='bookmarks')
     bookmark = models.BooleanField()
 
     def __str__(self):
