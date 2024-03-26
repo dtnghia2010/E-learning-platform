cd4cfcf64b85a7bce68e8fb4e48fb53632704863
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-03-26 22:54:14 +0700

    import Quizz models

--- Chapter/models.py
+++ Chapter/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from Document.models import Document
+# from quizz.models import Quizz
 
 class Chapter(models.Model):
     chapter_id = models.AutoField(primary_key=True)
     chapter_name = models.CharField(max_length=255)
     content = models.TextField()
-    #code = models.ForeignKey('Quiz', on_delete=models.CASCADE)
+    # code = models.ForeignKey(Quizz, on_delete=models.CASCADE)
     document_id = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='chapters')
 
     def __str__(self):
