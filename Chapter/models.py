5062bb1198dfb5d0fda9fd0b1e7fb71948124068
thuongngo050902 <ngothithuong592002@gmail.com>
2024-03-26 22:08:18 +0700

    Course Model

---
+++ Chapter/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from Document.models import Document
+from Quiz.models import Quiz
+
+# Create your models here.
+class Chapter(models.Model):
+    chapter_id = models.AutoField(primary_key=True)
+    chapter_name = models.CharField(max_length=255)
+    content = models.TextField()
+    code = models.ForeignKey('Quiz', on_delete=models.CASCADE)
+    document_id = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='chapters')
+
+    def __str__(self):
+        return self.chapter_name
