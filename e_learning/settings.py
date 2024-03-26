5aa270b6c252df6ae160121aa6aab56090f3af5d
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2024-03-26 11:51:47 +0700

    fix cors

--- e_learning/settings.py
+++ e_learning/settings.py
@@ -106,7 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:3000',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
