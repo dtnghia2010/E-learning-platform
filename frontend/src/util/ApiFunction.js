6ce6fe8c3146d70d80eeb8f804a7be74b23c494e
nathang0147 <nganhthang147@gmail.com>
2024-03-28 13:35:01 +0700

    feat: Fake API view course, view course by bookmark, delete course, view course bookmark

--- frontend/src/util/ApiFunction.js
+++ frontend/src/util/ApiFunction.js
@@ -1,15 +1,51 @@
 import axios from 'axios';
 
+export const getHeaders = () => {
+    const token = localStorage.getItem("access_token");
+    return {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+    }
+
+}
+
 export const apiFunction = axios.create({
     baseURL: 'http://localhost:8000',
 });
 
 export async function getCourses(){
     try {
-        const response = await apiFunction.get('/courses');
+        const response = await apiFunction.get('/courses',{
+            headers: getHeaders()
+        });
         return response.data;
     }catch (error){
         console.error(error);
         throw new Error('Error fetching courses');
+    }
+}
+
+export async function getCoursesByBookmark(){
+    try {
+        const response = await apiFunction.get('/bookmark', {
+            headers: getHeaders()
+        });
+        return response.data;
+    }catch (error){
+        console.error(error);
+        throw new Error('Error fetching bookmarked courses');
+    }
+}
+
+export async function deleteBookmark(courseId){
+    try {
+        const response = await apiFunction.delete(`/bookmark/${courseId}`, {
+            headers: getHeaders()
+
+        });
+        return response.data;
+    }catch (error){
+        console.error(error);
+        throw new Error('Error deleting bookmarked course');
     }
 }
