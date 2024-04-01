f96ae94bb239b40632a2205bc6e82adb22c9b2c7
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-04-01 16:41:05 +0700

    change database

--- Chapter/models.py
+++ Chapter/models.py
@@ -6,7 +6,8 @@ class Chapter(models.Model):
     chapter_id = models.AutoField(primary_key=True)
     chapter_name = models.CharField(max_length=255)
     content = models.TextField()
-    code = models.ForeignKey(Quizz, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6, unique=True)
+    quizz_id = models.ForeignKey(Quizz, on_delete=models.CASCADE)
     document_id = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='chapters')
     def __str__(self):
         return self.chapter_name
--- answerlist/models.py
+++ answerlist/models.py
@@ -4,7 +4,9 @@ from question.models import Question
 # Create your models here.
 class AnswerList(models.Model):
     answer_id = models.AutoField(primary_key=True)
-    answer = models.CharField(max_length=255)
+    answer1 = models.CharField(max_length=255)
+    answer2 = models.CharField(max_length=255)
+    answer3 = models.CharField(max_length=255)
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     def __str__(self):
