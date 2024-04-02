b138fa1267f27ea6030497f99cb603ab6ca2e691
Khoa <khoakhoa2812@gmail.com>
2024-04-02 16:30:54 +0700

    update

--- Course/urls.py
+++ Course/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CourseView, createCourse
 
 urlpatterns = [
-    path('category/<int:category_id>', CourseView.as_view()),
+    path('cate_<int:category_id>/', CourseView.as_view()),
     path('CreateCourse/', createCourse.as_view()),
 ]
