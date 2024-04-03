d02fa5e74169349625f3833fc950246d445bb6e4
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-04-03 22:30:06 +0700

    create API for Documents

--- Document/views.py
+++ Document/views.py
@@ -25,27 +25,29 @@ class DocumentView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, document_id):
+    def put(self, request, Document_id):
         try:
-            saved_document = Document.objects.get(pk=document_id)
+            saved_document = Document.objects.get(document_id=Document_id)
         except Document.DoesNotExist:
             return Response(
                 status=status.HTTP_404_NOT_FOUND
             )
         serializer = DocumentSerializer(instance=saved_document, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
+        if serializer.is_valid():
             serializer.save()
             return Response({
                 "success": "Document '{}' updated successfully".format(saved_document.document_name)
             })
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, document_id):
-        # Get object with this pk
-        document = Document.objects.get(pk=document_id)
-        document.delete()
-        return Response({
-            "message": "Document with id `{}` has been deleted.".format(document_id)
-        }, status=204)
+    def delete(self, request, Document_id):
+        try:
+            document = Document.objects.get(document_id=Document_id)
+            document.delete()
+            return Response({"message": "Document with id `{}` has been deleted.".format(Document_id)}, status=204)
+        except Document.DoesNotExist:
+            return Response(
+                status=status.HTTP_404_NOT_FOUND
+            )
 
 
