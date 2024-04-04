4e1f2ac51e83277f552413ff53e040b2be5c94d9
Khoa <khoakhoa2812@gmail.com>
2024-04-05 06:26:41 +0700

    update

--- Course/serializers.py
+++ Course/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 from .models import Course, Bookmark
+from category.models import Category
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    category_name = serializers.StringRelatedField(source='category_id')
+    category_name = serializers.CharField(write_only=True)
+    category_id = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Course
-        fields = ['course_id', 'course_name', 'category_name']
+        fields = ['course_id', 'course_name', 'category_name', 'category_id']
 
     def create(self, validated_data):
-        instance = self.Meta.model(**validated_data)
+        category_name = validated_data.pop('category_name')
+        category = Category.objects.get(category_name=category_name)
+        instance = self.Meta.model(category_id=category, **validated_data)
         instance.save()
         return instance
