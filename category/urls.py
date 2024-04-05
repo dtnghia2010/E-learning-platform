191ef14eb30bc957213f5d9fdd25e330ba55b45c
npkyphuong04 <kyphuong0414@gmail.com>
2024-04-05 13:04:09 +0700

    get all categories

---
+++ category/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CategoryList, CategoryDetail
+
+urlpatterns = [
+    path('category/', CategoryList.as_view()),
+    path('category/<int:category_id/', CategoryDetail.as_view()),
+]
--- category/views.py
+++ category/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
 from .models import Category
+from .serializers import CategorySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+import jwt
+from django.conf import settings
+from django.db.utils import IntegrityError
+
+
 # Create your views here.
+
+class CategoryList(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return Response({'error': 'Token expired'}, status=status.HTTP_401_UNAUTHORIZED)
+        except jwt.DecodeError:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except IntegrityError:
+                return Response({'error': 'Category name already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoryDetail(APIView):
+    def get_object(self, category_id):
+        try:
+            return Category.objects.get(category_id=category_id)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, category_id):
+        category = self.get_object(category_id)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def put(self, request, category_id):
+        category = self.get_object(category_id)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, category_id):
+        category = self.get_object(category_id)
+        category.delete()
+        return Response(status=status.HTTP_404_NOT_FOUND)
