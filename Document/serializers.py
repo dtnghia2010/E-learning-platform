46d6eda0d8f1c4e8a3be97bb3ec939ee661f176e
thuongngo050902 <ngothithuong592002@gmail.com>
2024-04-05 15:43:14 +0700

    getAPI_for_Document

--- Document/serializers.py
+++ Document/serializers.py
@@ -7,4 +7,9 @@ class DocumentSerializer(serializers.ModelSerializer):
         model = Document
         fields = "__all__"
 
+class DocumentAllSerializer(serializers.ModelSerializer):
+     class Meta:
+         model = Document
+         fields = ['document_name']
+
 
