3ba3330b7def1e74a3ce3e1b26e7d26ad314f89b
dtnghia2010 <ititiu21256@student.hcmiu.edu.vn>
2024-04-05 23:54:14 +0700

    complete course detail api

--- Course/urls.py
+++ Course/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CourseDetailView
 
 urlpatterns = [
-    path('course_detail/<int:course_id>/', CourseDetailView.as_view(), name='course_detail'),
+    path('course_detail/<int:course_id>/', CourseDetailView.as_view()),
 ]
