46e58791a80fc1e016535b9ae84a52fe70589d90
thuongngo050902 <ngothithuong592002@gmail.com>
2024-04-05 15:32:57 +0700

    getAPI_for_Document

--- e_learning/settings.py
+++ e_learning/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
    "default": {
        "ENGINE": "django.db.backends.postgresql",
        "NAME": "postgres",
-       "USER": "postgres.dbvfehsiwbewquyrgjzv",
-       "PASSWORD": "hRSw6LSXvGIGJa3T",
+       "USER": "postgres.qnaxcyfxsnrnxmddosfm",
+       "PASSWORD": "ABCD:12345:ABCD:12345",
        "HOST": "aws-0-ap-southeast-1.pooler.supabase.com",
        "PORT": "5432",
    }
