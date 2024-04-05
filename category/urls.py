c80d3d66a2a1e54294a91df9c7b078b679fb4170
npkyphuong04 <kyphuong0414@gmail.com>
2024-04-05 13:23:41 +0700

    fix urls of categories

--- category/urls.py
+++ category/urls.py
@@ -3,5 +3,5 @@ from .views import CategoryList, CategoryDetail
 
 urlpatterns = [
     path('category/', CategoryList.as_view()),
-    path('category/<int:category_id/', CategoryDetail.as_view()),
+    path('category/<int:category_id>/', CategoryDetail.as_view()),
 ]
