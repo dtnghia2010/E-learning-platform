7df65cbb00a8a7aee2283d487eeb92c7be5998c8
dtnghia2010 <ititiu21256@student.hcmiu.edu.vn>
2024-04-06 01:31:22 +0700

    update category model

--- Course/serializers.py
+++ Course/serializers.py
@@ -7,10 +7,10 @@ from authentication.models import User
 
 class CourseSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(write_only=True)
-    user_id = serializers.IntegerField(source='user_id', read_only=True)
+    # user_id = serializers.IntegerField(source='user_id', read_only=True)
     class Meta:
         model = Course
-        fields = ['course_id', 'course_name', 'category_name']
+        fields = ['course_id', 'course_name', 'category_name', 'user_id']
 
     def create(self, validated_data):
         category_name = validated_data.pop('category_name')
