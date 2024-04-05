ab09c8226b653b910d2dcdd2cc9002721d90f120
thuongngo050902 <ngothithuong592002@gmail.com>
2024-04-05 18:46:45 +0700

    getAPI_for_Document

---
+++ Course/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import CourseDetailView
+
+urlpatterns = [
+    path('course_detail/<int:course_id>/', CourseDetailView.as_view(), name='course_detail'),
+]
