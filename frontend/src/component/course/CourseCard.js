3bb9c238081c49df1ebab8003393bebd02768ea8
nathang0147 <nganhthang147@gmail.com>
2024-04-07 00:28:37 +0700

    feat: final touching course card

--- frontend/src/component/course/CourseCard.js
+++ frontend/src/component/course/CourseCard.js
@@ -1,15 +1,16 @@
 import {Card, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
 
 
 const CourseCard = ({course}) => {
     return (
         <>
-            <a href="#" className="h-32 sm:h-52 w-40 sm:w-64">
+            <Link to={`/document/${course.course_id}`} className="h-32 sm:h-52 w-40 sm:w-64 cursor-pointer">
                 <img src="/images/folderImg.png" alt="" className=" hover:shadow-xl"/>
                 <div>
                     <p className="card-title">{course.course_name} </p>
                 </div>
-            </a>
+            </Link>
         </>
     );
 };
