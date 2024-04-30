9bf0b49c3424a714baba0d1c138604d06d5c335a
thuongngo050902 <ngothithuong592002@gmail.com>
2024-04-30 13:22:15 +0700

    create Chapter

--- Chapter/urls.py
+++ Chapter/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ChapterView
+from .views import ChapterView, ChapterCreateView
 
 urlpatterns = [
     path('chapter/', ChapterView.as_view()),
     path('chapter/<int:chapter_id>/', ChapterView.as_view()),
+    path('ChapterCreate/', ChapterCreateView.as_view()),
 ]
