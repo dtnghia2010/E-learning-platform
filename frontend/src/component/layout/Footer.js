8f376c7165a0bef4ca795df4effa28dc689ab25d
nathang0147 <nganhthang147@gmail.com>
2024-05-02 16:16:29 +0700

    fix: footer in the end of viewport

--- frontend/src/component/layout/Footer.js
+++ frontend/src/component/layout/Footer.js
@@ -1,10 +1,14 @@
 import {Box, Grid, Typography} from "@mui/material";
 
+
 const Footer = () => {
     let today = new Date();
 
     return (
-        <Box component="footer" sx={{ bgcolor: 'dark', color: 'light', py: 3, mt: 5 }}>
+        <Box component="footer"
+             sx={{ bgcolor: 'dark', color: 'light', py: 3, mt: 5 }}
+             className="inset-x-0 bottom-0 text-center"
+             >
             <Grid container>
                 <Grid item xs={12} md={12}>
                     <Typography align="center" variant="body1">
