495e4a5dd87550128443a49392db4bfc83a306a6
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-05-10 15:49:05 +0700

    update courseID

--- Chapter/urls.py
+++ Chapter/urls.py
@@ -4,6 +4,7 @@ from.views import CreateChapter
 from.views import UpdateChapter
 urlpatterns = [
     path('chapter/', ChapterView.as_view()),
+    path('chapter/<int:chapter_id>/', ChapterView.as_view()),
     path('document/<int:document_id>/createchapter/', CreateChapter.as_view()),
     path('updateChapter/<int:chapter_id>/', UpdateChapter.as_view()),
 ]
