0bb2f4feba9a1a9fa5621d26d732ec59d77a89a9
Khoa <khoakhoa2812@gmail.com>
2024-05-14 10:07:57 +0700

    create quizz (create question and answer)

--- answerlist/views.py
+++ answerlist/views.py
@@ -24,4 +24,5 @@ class CreateAnswerList(APIView):
         serializer = CreateAnswerListSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        serializer.data['request'] = request.data
         return Response(serializer.data, status=status.HTTP_201_CREATED)
