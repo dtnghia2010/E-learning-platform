9c4f942f5142b1836609738b7fa5971b1243b786
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-05-14 13:54:19 +0700

    add authenticate

--- quizz/views.py
+++ quizz/views.py
@@ -9,19 +9,19 @@ from rest_framework.permissions import IsAuthenticated
 
 class QuizzByCode(APIView):
     def get(self, request, quizz_code):
-        # auth_header = request.META.get('HTTP_AUTHORIZATION')
-        # if not auth_header or not auth_header.startswith('Bearer '):
-        #     print(auth_header)
-        #
-        #     raise AuthenticationFailed('Unauthenticated!')
-        # token = auth_header.split(' ')[1]
-        #
-        # try:
-        #     payload = jwt.decode(token, 'secret', algorithms=['HS256'])
-        # except jwt.ExpiredSignatureError:
-        #     raise AuthenticationFailed('Authentication token expired!')
-        # except jwt.InvalidTokenError:
-        #     raise AuthenticationFailed('Invalid authentication token!')
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+        if not auth_header or not auth_header.startswith('Bearer '):
+            print(auth_header)
+
+            raise AuthenticationFailed('Unauthenticated!')
+        token = auth_header.split(' ')[1]
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Authentication token expired!')
+        except jwt.InvalidTokenError:
+            raise AuthenticationFailed('Invalid authentication token!')
         try:
             quizz = Quizz.objects.get(code=quizz_code)
         except Quizz.DoesNotExist:
