c5aa546b5c3f815169d57617f3170dfc06fd4630
thuongngo050902 <ngothithuong592002@gmail.com>
2024-05-15 11:31:17 +0700

    AllQuizzesByUser

--- quizz/views.py
+++ quizz/views.py
@@ -7,3 +7,34 @@ from rest_framework import status
 # Create your views here.
 # class QuizView(APIView):
 #     def get(self):
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth import get_user_model
+from .models import Quizz
+from .serializers import QuizzSerializer
+import jwt
+
+User = get_user_model()
+
+class GetAllQuizzesByUser(APIView):
+    def get(self, request, user_id=None):
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+        if not auth_header or not auth_header.startswith('Bearer '):
+            print(auth_header)
+            raise AuthenticationFailed('Unauthenticated!')
+        token = auth_header.split(' ')[1]
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Authentication token expired!')
+        except jwt.InvalidTokenError:
+            raise AuthenticationFailed('Invalid authentication token!')
+
+        user = User.objects.filter(id=payload['id']).first()
+
+        quizzes = Quizz.objects.filter(user_id=user.id)
+        serializer = QuizzesViewByUserSerializer(quizzes, many=True)
+        return Response(serializer.data)
+
