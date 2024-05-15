e3c9f10498df0e5a542b3a31f8cdc4a5a8663420
thuongngo050902 <ngothithuong592002@gmail.com>
2024-05-15 12:06:40 +0700

    AllQuizzesByUser

--- quizz/views.py
+++ quizz/views.py
@@ -12,8 +12,13 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from django.contrib.auth import get_user_model
 from .models import Quizz
-from .serializers import QuizzSerializer
+from .serializers import QuizzSerializer, QuizzesViewByUserSerializer
 import jwt
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Quizz
+from rest_framework.exceptions import AuthenticationFailed
 
 User = get_user_model()
 
@@ -22,6 +27,7 @@ class GetAllQuizzesByUser(APIView):
         auth_header = request.META.get('HTTP_AUTHORIZATION')
         if not auth_header or not auth_header.startswith('Bearer '):
             print(auth_header)
+
             raise AuthenticationFailed('Unauthenticated!')
         token = auth_header.split(' ')[1]
 
@@ -34,7 +40,33 @@ class GetAllQuizzesByUser(APIView):
 
         user = User.objects.filter(id=payload['id']).first()
 
-        quizzes = Quizz.objects.filter(user_id=user.id)
-        serializer = QuizzesViewByUserSerializer(quizzes, many=True)
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        print(request.data)
+        quizz = Quizz.objects.filter(user_id=user.id)
+        serializer = QuizzesViewByUserSerializer(quizz, many=True)
         return Response(serializer.data)
+
+
+# class GetAllQuizzesByUser(APIView):
+#     def get(self, request, user_id=None):
+#         auth_header = request.META.get('HTTP_AUTHORIZATION')
+#         if not auth_header or not auth_header.startswith('Bearer '):
+#             print(auth_header)
+#             raise AuthenticationFailed('Unauthenticated!')
+#         token = auth_header.split(' ')[1]
+#
+#         try:
+#             payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+#         except jwt.ExpiredSignatureError:
+#             raise AuthenticationFailed('Authentication token expired!')
+#         except jwt.InvalidTokenError:
+#             raise AuthenticationFailed('Invalid authentication token!')
+#
+#         user = User.objects.filter(id=payload['id']).first()
+#
+#         quizzes = Quizz.objects.filter(user_id=user.id)
+#         serializer = QuizzesViewByUserSerializer(quizzes, many=True)
+#         return Response(serializer.data)
 
