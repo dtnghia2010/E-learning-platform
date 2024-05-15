21412f0bfd81722afc7f0102831d9da22f25147b
thuongngo050902 <ngothithuong592002@gmail.com>
2024-05-15 11:31:08 +0700

    AllQuizzesByUser

--- quizz/serializers.py
+++ quizz/serializers.py
@@ -5,3 +5,8 @@ class QuizzSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quizz
         fields = '__all__'
+
+class QuizzesViewByUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quizz
+        fields = ['quizz_name', 'quizz_id']
