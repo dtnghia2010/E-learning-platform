bd8044721aac9e1433494dfe10da67657ed2ee73
thuongngo050902 <ngothithuong592002@gmail.com>
2024-05-15 11:31:13 +0700

    AllQuizzesByUser

--- quizz/urls.py
+++ quizz/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from.views import GetAllQuizzesByUser
 
 urlpatterns = [
-    path('quizz', ),
-    path('quizz/<int:quizz_id>', ),
-    path('quizz/<int:quizz_id>/questions/', )
+    # path('quizz', ),
+    # path('quizz/<int:quizz_id>', ),
+    # path('quizz/<int:quizz_id>/questions/', ),
+    path('getAllQuizzesByUser/', GetAllQuizzesByUser.as_view()),
 ]
