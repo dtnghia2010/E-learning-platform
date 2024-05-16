4e8488ee1f1240d5dac91c915a790b631f8ed2df
npkyphuong04 <kyphuong0414@gmail.com>
2024-05-16 11:10:04 +0700

    fix update

--- question/urls.py
+++ question/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 # from quizz.models import Quizz
-from .views import QuestionListByQuizz, ResultsByQuizz, QuestionAndAnswerListCreate
+from .views import QuestionListByQuizz, ResultsByQuizz, QuestionAndAnswerListCreate, QuestionAndAnswerListUpdate
 from answerlist.views import CreateAnswerList
 urlpatterns = [
     path('quizz/<int:quizz_id>/questions/', QuestionListByQuizz.as_view()),
     path('quizz/<int:quizz_id>/results/', ResultsByQuizz.as_view()),
-    path('quizz/<int:quizz_id>/createQuestion/', QuestionAndAnswerListCreate.as_view())
+    path('quizz/<int:quizz_id>/createQuestion/', QuestionAndAnswerListCreate.as_view()),
+    path('quizz/<int:quizz_id>/updateQuestion/<int:question_id>/', QuestionAndAnswerListUpdate.as_view()),
     # path('quizz/<int:quizz_id>/questions/<int:question_id>', )
 ]
--- question/views.py
+++ question/views.py
@@ -91,3 +91,39 @@ class QuestionAndAnswerListCreate(APIView):
                 return Response(answer_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             return Response(question_serializer.data, status=status.HTTP_201_CREATED)
         return Response(question_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionAndAnswerListUpdate(APIView):
+    def put(self, request, quizz_id, question_id):
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+        if not auth_header or not auth_header.startswith('Bearer '):
+            raise AuthenticationFailed('Unauthenticated!')
+        token = auth_header.split(' ')[1]
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Authentication token expired!')
+        except jwt.InvalidTokenError:
+            raise AuthenticationFailed('Invalid authentication token!')
+
+        question = Question.objects.get(question_id=question_id)
+        question_serializer = QuestionAnswerListSerializer(question, data=request.data, context={'quizz': quizz_id}, partial=True)
+        if question_serializer.is_valid():
+            question_serializer.save()
+        else:
+            return Response(question_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        answer_list = AnswerList.objects.get(question_id=question_id)
+        answer1 = request.data.get('answer1')
+        answer2 = request.data.get('answer2')
+        answer3 = request.data.get('answer3')
+        correct_answer = answer1 if "&True" in answer1 else answer2 if "&True" in answer2 else answer3
+        question.answer = correct_answer
+        question.save()
+        answer_serializer = CreateAnswerListSerializer(answer_list, data=request.data, partial=True)
+        if answer_serializer.is_valid():
+            answer_serializer.save()
+        else:
+            return Response(answer_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(question_serializer.data, status=status.HTTP_200_OK)
