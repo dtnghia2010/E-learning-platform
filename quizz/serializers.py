4f2fa90488e46cfa369946b5a776a283b3441ea8
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-05-18 22:45:21 +0700

    update

--- quizz/serializers.py
+++ quizz/serializers.py
@@ -15,14 +15,17 @@ class QuizzesViewByUserSerializer(serializers.ModelSerializer):
         fields = ['quizz_name', 'quizz_id']
 
 class QuizzCreateSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(write_only=True)
     class Meta:
         model = Quizz
-        fields = ['quizz_id','category_id', 'quizz_name', 'code', 'user_id']
+        fields = ['quizz_id','category_name', 'quizz_name', 'code', 'user_id']
     def create(self, validated_data):
         print(validated_data)
         # category_id = validated_data.pop('category_id', None)
+        category_name = validated_data.pop('category_name', None)
+        category = get_object_or_404(Category, category_name=category_name)
         username = validated_data.pop('user_id', None)
         user = get_object_or_404(User, username=username)
-        return Quizz.objects.create(user_id=user, **validated_data)
+        return Quizz.objects.create(user_id=user, category_id=category,**validated_data)
         # category = get_object_or_404(Category, category_name=category_id)
 
