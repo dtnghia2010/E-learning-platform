cbfc1b98fc917bf2dbd3605a8b8f3628d56dba99
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-05-31 16:39:40 +0700

    update chapter content css

--- frontend/src/page/chapter/Chapter.js
+++ frontend/src/page/chapter/Chapter.js
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {getChapter} from "../../util/ApiFunction";
+import {useNavigate} from "react-router-dom";
 
 const CustomDivider = () => {
     return (
@@ -37,23 +38,32 @@ const Chapter = () => {
             throw  e
         }
     }
-
+    const navigator= useNavigate();
     useEffect(()=>{
         handleGetChapter()
     },[])
     return (
         <div>
             <header className="flex-row justify-center items-center">
+                <button className="rounded-lg bg-myBlue p-3 absolute left-56 back-to-content hover:bg-myGray  "
+                        onClick={()=>{navigator(`/lecture/${chapter.document_id}`)}}>  
+                <span><i class="fa-solid fa-arrow-left mr-2"></i></span> 
+                <span>Back to Content</span>  </button>
+                <button className="rounded-lg bg-myBlue p-3 absolute right-56 next-lecture hover:bg-myGray  ">  
+                <span>Next Lecture</span>
+                <span><i class="fa-solid fa-arrow-right ml-2"></i></span>  </button>
                 <div className="text-2xl font-bold">
-                    Lecture {chapter.chapter_id}: {chapter.chapter_name}
+                   <h1 className="text-center mb-8 mt-5">{chapter.chapter_name}</h1> 
                 </div>
+
             </header>
             <CustomDivider/>
-            <div className="chapter-content">
-                {chapter.content}
+            <div className="chapter-content px-20">
+            {chapter.content.split('\n').map((line, index) => (
+                <p key={index} className="px-32">{line}</p>
+            ))}
             </div>
         </div>
-
     );
 };
 
