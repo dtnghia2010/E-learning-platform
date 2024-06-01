e94a24d887451a3fe425ad929cab863a254bca43
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-06-01 22:27:07 +0700

    change color

--- frontend/src/page/Login.js
+++ frontend/src/page/Login.js
@@ -39,8 +39,8 @@ const Login = () => {
 
     return (
         <div>
-        <div className="flex items-center justify-center  w-full h-screen  bg-gray-100">
-            <div className="bg-gray-tone  rounded-lg  px-4 py-10 max-w-sm mx-auto w-full h-screen ">
+        <div className="flex items-center justify-center  w-full h-screen  bg-myWhite">
+            <div className="bg-myLightYellow  rounded-lg  px-4 py-10 max-w-sm mx-auto w-full h-screen ">
                 <h1 className="text-5xl font-bold text-center mb-6">Login</h1>
                 <p className="font-semibold text-center text-black mt-4 mb-10"> Welcome back, Please log in to your account</p>
                 <form action="#">
@@ -51,7 +51,7 @@ const Login = () => {
                             id="username"
                             value={username}
                             onChange={handleInputUsername}
-                            className="w-full px-3 py-2 rounded-lg bg-gray-200 border focus:outline-none focus:ring-indigo-500 focus:ring-1"
+                            className="w-full px-3 py-2 rounded-lg bg-[#ffffff] border focus:outline-none focus:ring-indigo-500 focus:ring-1"
                         />
                     </div>
                     <div className="mb-6">
@@ -63,7 +63,7 @@ const Login = () => {
                             id="password"
                             value={password}
                             onChange={handleInputPassword}
-                            className="w-full px-3 py-2 rounded-lg bg-gray-200 border focus:outline-none focus:ring-indigo-500 focus:ring-1"
+                            className="w-full px-3 py-2 rounded-lg bg-[#ffffff] border focus:outline-none focus:ring-indigo-500 focus:ring-1"
                         />
                     </div>
                     <div className="flex justify-between items-center mb-6">
--- frontend/src/page/Register.js
+++ frontend/src/page/Register.js
@@ -62,8 +62,8 @@ export default  function Register(){
 
         //remove Header custom
         <div>
-       <div className="flex items-center justify-center  w-full h-screen  bg-gray-100">
-        <div className="bg-gray-tone  rounded-lg  px-4 py-10 max-w-sm mx-auto w-full h-screen ">
+       <div className="flex items-center justify-center  w-full h-screen  bg-myWhite">
+        <div className="bg-myLightYellow  rounded-lg  px-4 py-10 max-w-sm mx-auto w-full h-screen ">
             <h1 className="text-5xl font-bold text-center mb-6">Sign up</h1>
             <p className="font-semibold text-center text-black mt-4 mb-10">Create an account to unlock exclusive features</p>
             <form >
@@ -73,7 +73,7 @@ export default  function Register(){
                         type="email"
                         id="email"
                         value={email}
-                        className="w-full px-3 py-2 rounded-lg bg-gray-200 border focus:outline-none focus:ring-indigo-500 focus:ring-1"
+                        className="w-full px-3 py-2 rounded-lg bg-[#ffffff] border focus:outline-none focus:ring-indigo-500 focus:ring-1"
                         onChange={handleInputEmail}
                     />
                 </div>
@@ -84,7 +84,7 @@ export default  function Register(){
                         id="email"
                         value={username}
                         onChange={handleInputUsername}
-                        className="w-full px-3 py-2 rounded-lg bg-gray-200 border focus:outline-none focus:ring-indigo-500 focus:ring-1"
+                        className="w-full px-3 py-2 rounded-lg bg-[#ffffff] border focus:outline-none focus:ring-indigo-500 focus:ring-1"
                     />
                 </div>
                 <div className="mb-6">
@@ -96,7 +96,7 @@ export default  function Register(){
                         id="password"
                         value={password}
                         onChange={handleInputPassword}
-                        className="w-full px-3 py-2 rounded-lg bg-gray-200 border focus:outline-none focus:ring-indigo-500 focus:ring-1"
+                        className="w-full px-3 py-2 rounded-lg bg-[#ffffff] border focus:outline-none focus:ring-indigo-500 focus:ring-1"
                     />
                 </div>
                 <div className="mb-6">
@@ -108,7 +108,7 @@ export default  function Register(){
                         id="password"
                         value={confirmPassword}
                         onChange={handleInputConfirm}
-                        className="w-full px-3 py-2 rounded-lg bg-gray-200 border focus:outline-none focus:ring-indigo-500 focus:ring-1"
+                        className="w-full px-3 py-2 rounded-lg bg-[#ffffff] border focus:outline-none focus:ring-indigo-500 focus:ring-1"
                     />
                 </div>
                 <div className="flex justify-between items-center mb-6">
